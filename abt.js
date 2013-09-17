d1426c6e86c5566b351ac5a99c0fd42c7acf1474
Eli Dickinson <eli@elidickinson.com>
2013-09-17 17:45:27 -0400

    don't autorun callback any more and tighten up code and examples

--- abt.js
+++ abt.js
@@ -1,26 +1,24 @@
 function ad_block_test(callback, testad_id) {
+	if(typeof document.body == 'undefined') {
+		// right now just silently fail if the body element isn't there
+		return;
+	}
 	var version = "0.1";
 	var testad_id = testad_id ? testad_id : "sponsored-ad";
 	var testad = document.createElement("DIV");
 	testad.id = testad_id;
 	testad.style.position = "absolute";
 	testad.style.left = "-999px";
-	testad.appendChild(document.createTextNode("&nbsp;"))
+	testad.appendChild(document.createTextNode("&nbsp;"));
 	document.body.appendChild(testad); // add test ad to body
 
 	// wait a bit and then check its height
 	setTimeout(function() {
 		if (testad) {
-			callback(testad && (testad.clientHeight == 0), version);
+			callback((testad.clientHeight == 0), version);
 			document.body.removeChild(testad);
 			// Should testad disappearing entirely count as an ad block?
 			// Currently it does not fire callback at all in this case
 		}
-	}, 100);
-}
-
-// will automatically run and call the callback if its defined.
-// Otherwise gotta call it and pass a callback function that receives a bool yourself.
-if (typeof ad_block_test_callback == "function") {
-	ad_block_test(ad_block_test_callback);
+	}, 125);
 }
