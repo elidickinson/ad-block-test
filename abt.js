3b19892b102447a6768ec8eb7de95e020f893336
Eli Dickinson <eli@elidickinson.com>
2013-09-29 18:41:04 -0400

    better detection for ABP that leaves placeholders

--- abt.js
+++ abt.js
@@ -3,7 +3,7 @@ function ad_block_test(callback, testad_id) {
 		// right now just silently fail if the body element isn't there
 		return;
 	}
-	var version = "0.1-dev";
+	var version = "0.1.1-dev";
 	var testad_id = testad_id ? testad_id : "sponsored-ad";
 	var testad = document.createElement("DIV");
 	testad.id = testad_id;
@@ -15,8 +15,16 @@ function ad_block_test(callback, testad_id) {
 	// wait a bit and then check its height
 	setTimeout(function() {
 		if (testad) {
-			callback((testad.clientHeight == 0), version);
-			document.body.removeChild(testad);
+			var blocked = (testad.clientHeight == 0);
+			try {
+				// AdBlock Plus or Ad Block Edge without placeholders uses the following property to hide elements
+				blocked = blocked || (getComputedStyle(testad).getPropertyValue('-moz-binding') != "");
+			} catch (err) {
+				// log errors
+				if(console && console.log) { console.log("ad-block-test error",err); }
+			}
+			callback(blocked, version);
+			// document.body.removeChild(testad);
 			// Should testad disappearing entirely count as an ad block?
 			// Currently it does not fire callback at all in this case
 		}
